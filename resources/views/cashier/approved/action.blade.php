be11b21da50a798642fbea5d5353fd23195b3712
rachmanj <rachmanj@gmail.com>
2023-12-01 15:33:03 +0800

    minor update

--- resources/views/cashier/approved/action.blade.php
+++ resources/views/cashier/approved/action.blade.php
@@ -1,6 +1,6 @@
-<form action="{{ route('cashier.approveds.auto_outgoing', $model->id) }}" method="POST" id="auto-outgoing">
+<form action="{{ route('cashier.approveds.auto_outgoing', $model->id) }}" method="POST">
   @csrf @method('PUT')
+  <button type="submit" class="btn btn-xs btn-success {{ $model->outgoings->count() > 0 ? 'disabled' : '' }}" onclick="return confirm('Are You sure You want to pay this payreq?')">auto</button>
+  <a href="{{ route('cashier.approveds.pay', $model->id) }}" class="btn btn-xs btn-info">pay</a>
 </form>  
 
-<button type="submit" class="btn btn-xs btn-success {{ $model->outgoings->count() > 0 ? 'disabled' : '' }}" form="auto-outgoing" onclick="return confirm('Are You sure You want to pay this payreq?')">auto</button>
-<a href="{{ route('cashier.approveds.pay', $model->id) }}" class="btn btn-xs btn-info">pay</a>
