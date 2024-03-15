0a9f7d64cc6e8b437f216c30ed000450146c9e52
rachmanj <rachmanj@gmail.com>
2024-03-15 20:39:45 +0800

    minor update

--- resources/views/accounting/loans/action.blade.php
+++ resources/views/accounting/loans/action.blade.php
@@ -1,8 +1,10 @@
 @hasanyrole('superadmin|admin|cashier')
+<a class="btn btn-xs btn-warning" href="{{ route('accounting.loans.edit', $model->id) }}" class="btn btn-xs btn-primary">edit</a>
+<a class="btn btn-xs btn-success" href="{{ route('accounting.loans.show', $model->id) }}" class="btn btn-xs btn-primary">installment</a>
 <form action="{{ route('accounting.loans.destroy', $model->id) }}" method="POST">
   @csrf @method('DELETE')
-    <a class="btn btn-xs btn-warning" href="{{ route('accounting.loans.edit', $model->id) }}" class="btn btn-xs btn-primary">edit</a>
-    <a class="btn btn-xs btn-success" href="{{ route('accounting.loans.show', $model->id) }}" class="btn btn-xs btn-primary">installment</a>
+  @if($model->installments->count() == 0)
   <button type="submit" class="btn btn-xs btn-danger" onclick="return confirm('Are You sure You want to delete this record?')">delete</button>
+  @endif
 </form>
 @endhasanyrole
