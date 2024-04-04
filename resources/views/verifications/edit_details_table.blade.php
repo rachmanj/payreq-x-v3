6f9d0c59e439d001f555472bd6693f0627d8d8cd
rachmanj <rachmanj@gmail.com>
2024-04-04 11:13:30 +0800

    minor update

--- resources/views/verifications/edit_details_table.blade.php
+++ resources/views/verifications/edit_details_table.blade.php
@@ -17,7 +17,7 @@
                             <th>New Account</th>
                             <td></td>
 
-                            @hasanyrole('superadmin|admin|cashier|cashier_017')
+                            @hasanyrole('superadmin|admin|cashier|cashier_017|cashier_bo')
                             <th>Project</th>
                             <th>Dept</th>
                             @endhasanyrole
@@ -52,7 +52,7 @@
                                         </div> 
                                     </td>
 
-                                    @hasanyrole('superadmin|admin|cashier|cashier_017')
+                                    @hasanyrole('superadmin|admin|cashier|cashier_017|cashier_bo')
                                     <td>
                                         <select name="realization_details[{{ $key }}][project]" class="form-control">
                                             @foreach ($projects as $project)
