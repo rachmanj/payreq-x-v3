77735247af0c7df63c6c4fc6efccef5c9bb67e65
rachmanj <rachmanj@gmail.com>
2024-05-14 13:58:54 +0800

    minor update

--- app/Http/Controllers/Accounting/SapSyncController.php
+++ app/Http/Controllers/Accounting/SapSyncController.php
@@ -33,7 +33,7 @@ class SapSyncController extends Controller
             ->get()
             ->map(function ($detail) {
                 $account = Account::where('account_number', $detail->account_code)->first();
-                $detail->account_name = $account->account_name;
+                $detail->account_name = $account ? $account->account_name : "not found";
                 return $detail;
             });
 
