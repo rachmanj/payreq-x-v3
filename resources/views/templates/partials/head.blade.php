e9b75063ccc82ddd90eee167682cabe5a6c1d8e8
rachmanj <rachmanj@gmail.com>
2024-05-16 16:24:31 +0800

    disable search google font

--- resources/views/templates/partials/head.blade.php
+++ resources/views/templates/partials/head.blade.php
@@ -4,7 +4,7 @@
     <title>PayReq System</title>
   
     <!-- Google Font: Source Sans Pro -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    {{-- <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback"> --}}
     <!-- Font Awesome Icons -->
     <link rel="stylesheet" href="{{ asset('adminlte/plugins/fontawesome-free/css/all.min.css') }}">
     <!-- Theme style -->
--- resources/views/user-payreqs/realizations/print_pdf.blade.php
+++ resources/views/user-payreqs/realizations/print_pdf.blade.php
@@ -6,7 +6,7 @@
   <title>PayReq System | Print Realization Payreq</title>
 
   <!-- Google Font: Source Sans Pro -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  {{-- <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback"> --}}
   <!-- Font Awesome -->
   <link rel="stylesheet" href="{{ asset('adminlte/plugins/fontawesome-free/css/all.min.css') }}">
   
