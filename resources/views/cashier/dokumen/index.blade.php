84a10a8f62328c98fe0e0c20b0d3eb750ab0863b
rachmanj <rachmanj@gmail.com>
2024-10-22 09:13:41 +0800

    akses to upload dokumen

--- resources/views/cashier/dokumen/index.blade.php
+++ resources/views/cashier/dokumen/index.blade.php
@@ -1,7 +1,7 @@
 @extends('templates.main')
 
 @section('title_page')
-  REKENING KORAN  
+    REKENING KORAN
 @endsection
 
 @section('breadcrumb_title')
@@ -9,155 +9,175 @@
 @endsection
 
 @section('content')
-<div class="row">
-  <div class="col-12">
-
-    <div class="card">
-      <div class="card-header">
-        <a href="#" class="text-dark">REKENING KORAN</a> | <a href="{{ route('cashier.dokumen.index', ['page' => 'pcbc']) }}">PCBC</a>
-        @can('upload_dokumen')
-          <button href="#" class="btn btn-xs btn-success float-right mr-2" data-toggle="modal" data-target="#modal-upload"><i class="fas fa-upload"></i> Upload RK</button>
-        @endcan
-      </div>  <!-- /.card-header -->
-     
-      <div class="card-body">
-        <table id="koran" class="table table-bordered table-striped">
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>Bank Account</th>
-              <th>Project</th>
-              <th>Periode</th>
-              <th>Reconciled</th>
-              <th>Uploaded by</th>
-              <th></th>
-            </tr>
-          </thead>
-        </table>
-      </div> <!-- /.card-body -->
-    </div> <!-- /.card -->
-  </div> <!-- /.col -->
-</div>  <!-- /.row -->
-
-{{-- Modal create --}}
-<div class="modal fade" id="modal-upload">
-  <div class="modal-dialog modal-md">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h4 class="modal-title"> Upload Rekening Koran</h4>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <form action="{{ route('cashier.dokumen.upload') }}" method="POST" enctype="multipart/form-data">
-        @csrf
-      <div class="modal-body">
-
-        <div class="row">
-          <div class="col-12">
-            <div class="form-group">
-              <label for="periode">Periode</label>
-              {{-- <input type="hidden" name="project" value="{{ auth()->user()->project }}"> --}}
-              <input type="hidden" name="type" value="koran">
-              <input type="month" name="periode" id="periode" class="form-control @error('periode') is-invalid @enderror">
-              @error('periode')
-                <div class="invalid-feedback">
-                  {{ $message }}
-                </div>
-              @enderror
-            </div>
-          </div>
-        </div>
-
+    <div class="row">
         <div class="col-12">
-          <div class="form-group">
-            <label for="giro_id">Giro</label>
-            <select name="giro_id" id="giro_id" class="form-control select2bs4">
-              @foreach ($giros as $giro)
-                  <option value="{{ $giro->id }}">{{ $giro->acc_no . ' - ' . $giro->acc_name }}</option>
-              @endforeach
-            </select>
-          </div>
-        </div>
 
-        <div class="row">
-          <div class="col-12">
-            <div class="form-group">
-              <label for="file_upload">Dokumen File</label>
-              <input type="file" name="file_upload" id="file_upload" class="form-control">
-            </div>
-          </div>
-        </div>
+            <div class="card">
+                <div class="card-header">
+                    <a href="#" class="text-dark">REKENING KORAN</a> @can('upload_pcbc')
+                        | <a href="{{ route('cashier.dokumen.index', ['page' => 'pcbc']) }}">PCBC</a>
+                    @endcan
+                    @can('upload_dokumen')
+                        <button href="#" class="btn btn-xs btn-success float-right mr-2" data-toggle="modal"
+                            data-target="#modal-upload"><i class="fas fa-upload"></i> Upload RK</button>
+                    @endcan
+                </div> <!-- /.card-header -->
 
-        <div class="form-group">
-          <label for="remarks">Remarks</label>
-          <textarea name="remarks" id="remarks" class="form-control"></textarea>
-        </div>
+                <div class="card-body">
+                    <table id="koran" class="table table-bordered table-striped">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Bank Account</th>
+                                <th>Project</th>
+                                <th>Periode</th>
+                                <th>Reconciled</th>
+                                <th>Uploaded by</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                    </table>
+                </div> <!-- /.card-body -->
+            </div> <!-- /.card -->
+        </div> <!-- /.col -->
+    </div> <!-- /.row -->
 
-      </div> <!-- /.modal-body -->
-      <div class="modal-footer float-left">
-        <button type="button" class="btn btn-sm btn-default" data-dismiss="modal"> Close</button>
-        <button type="submit" class="btn btn-sm btn-primary"><i class="fas fa-save"></i> Save</button>
-      </div>
-    </form>
-    </div> <!-- /.modal-content -->
-  </div> <!-- /.modal-dialog -->
-</div>
+    {{-- Modal create --}}
+    <div class="modal fade" id="modal-upload">
+        <div class="modal-dialog modal-md">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title"> Upload Rekening Koran</h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <form action="{{ route('cashier.dokumen.upload') }}" method="POST" enctype="multipart/form-data">
+                    @csrf
+                    <div class="modal-body">
+
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="form-group">
+                                    <label for="periode">Periode</label>
+                                    {{-- <input type="hidden" name="project" value="{{ auth()->user()->project }}"> --}}
+                                    <input type="hidden" name="type" value="koran">
+                                    <input type="month" name="periode" id="periode"
+                                        class="form-control @error('periode') is-invalid @enderror">
+                                    @error('periode')
+                                        <div class="invalid-feedback">
+                                            {{ $message }}
+                                        </div>
+                                    @enderror
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-12">
+                            <div class="form-group">
+                                <label for="giro_id">Giro</label>
+                                <select name="giro_id" id="giro_id" class="form-control select2bs4">
+                                    @foreach ($giros as $giro)
+                                        <option value="{{ $giro->id }}">{{ $giro->acc_no . ' - ' . $giro->acc_name }}
+                                        </option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="form-group">
+                                    <label for="file_upload">Dokumen File</label>
+                                    <input type="file" name="file_upload" id="file_upload" class="form-control">
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="remarks">Remarks</label>
+                            <textarea name="remarks" id="remarks" class="form-control"></textarea>
+                        </div>
+
+                    </div> <!-- /.modal-body -->
+                    <div class="modal-footer float-left">
+                        <button type="button" class="btn btn-sm btn-default" data-dismiss="modal"> Close</button>
+                        <button type="submit" class="btn btn-sm btn-primary"><i class="fas fa-save"></i> Save</button>
+                    </div>
+                </form>
+            </div> <!-- /.modal-content -->
+        </div> <!-- /.modal-dialog -->
+    </div>
 @endsection
 
 @section('styles')
     <!-- DataTables -->
-  <link rel="stylesheet" href="{{ asset('adminlte/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
-  <link rel="stylesheet" href="{{ asset('adminlte/plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}">
-  <link rel="stylesheet" href="{{ asset('adminlte/plugins/datatables-buttons/css/buttons.bootstrap4.min.css') }}">
-  <link rel="stylesheet" type="text/css" href="{{ asset('adminlte/plugins/datatables/css/datatables.min.css') }}"/>
-  <!-- Select2 -->
-  <link rel="stylesheet" href="{{ asset('adminlte/plugins/select2/css/select2.min.css') }}">
-  <link rel="stylesheet" href="{{ asset('adminlte/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('adminlte/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('adminlte/plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('adminlte/plugins/datatables-buttons/css/buttons.bootstrap4.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('adminlte/plugins/datatables/css/datatables.min.css') }}" />
+    <!-- Select2 -->
+    <link rel="stylesheet" href="{{ asset('adminlte/plugins/select2/css/select2.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('adminlte/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css') }}">
 @endsection
 
 @section('scripts')
     <!-- DataTables  & Plugins -->
-<script src="{{ asset('adminlte/plugins/datatables/jquery.dataTables.min.js') }}"></script>
-<script src="{{ asset('adminlte/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
-<script src="{{ asset('adminlte/plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
-<script src="{{ asset('adminlte/plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
-<script src="{{ asset('adminlte/plugins/datatables/datatables.min.js') }}"></script>
-<!-- Select2 -->
-<script src="{{ asset('adminlte/plugins/select2/js/select2.full.min.js') }}"></script>
+    <script src="{{ asset('adminlte/plugins/datatables/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('adminlte/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('adminlte/plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
+    <script src="{{ asset('adminlte/plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('adminlte/plugins/datatables/datatables.min.js') }}"></script>
+    <!-- Select2 -->
+    <script src="{{ asset('adminlte/plugins/select2/js/select2.full.min.js') }}"></script>
 
-<script>
-  $(function () {
-    $("#koran").DataTable({
-      processing: true,
-      serverSide: true,
-      ajax: '{{ route('cashier.dokumen.data') . '?type=koran' }}',
-      columns: [
-        {data: 'DT_RowIndex', orderable: false, searchable: false},
-        {data: 'account'},
-        {data: 'account_project'},
-        {data: 'periode'},
-        {data: 'reconciled'},
-        {data: 'created_by'},
-        {data: 'action', orderable: false, searchable: false},
-      ],
-      fixedHeader: true,
-      columnDefs: [
-              {
-                "targets": [4],
-                "className": "text-center"
-              }
-            ]
-    })
+    <script>
+        $(function() {
+            $("#koran").DataTable({
+                processing: true,
+                serverSide: true,
+                ajax: '{{ route('cashier.dokumen.data') . '?type=koran' }}',
+                columns: [{
+                        data: 'DT_RowIndex',
+                        orderable: false,
+                        searchable: false
+                    },
+                    {
+                        data: 'account'
+                    },
+                    {
+                        data: 'account_project'
+                    },
+                    {
+                        data: 'periode'
+                    },
+                    {
+                        data: 'reconciled'
+                    },
+                    {
+                        data: 'created_by'
+                    },
+                    {
+                        data: 'action',
+                        orderable: false,
+                        searchable: false
+                    },
+                ],
+                fixedHeader: true,
+                columnDefs: [{
+                    "targets": [4],
+                    "className": "text-center"
+                }]
+            })
 
-     //Initialize Select2 Elements
-     $('.select2').select2()
+            //Initialize Select2 Elements
+            $('.select2').select2()
 
-    //Initialize Select2 Elements
-    $('.select2bs4').select2({
-      theme: 'bootstrap4'
-    })
+            //Initialize Select2 Elements
+            $('.select2bs4').select2({
+                theme: 'bootstrap4'
+            })
 
-  });
-</script>
+        });
+    </script>
 @endsection
--- resources/views/templates/partials/menu/user-payreq.blade.php
+++ resources/views/templates/partials/menu/user-payreq.blade.php
@@ -1,18 +1,22 @@
 <li class="nav-item dropdown">
-    <a id="dropdownPayreq" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link dropdown-toggle">My PayReqs</a>
+    <a id="dropdownPayreq" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"
+        class="nav-link dropdown-toggle">My PayReqs</a>
     <ul aria-labelledby="dropdownPayreq" class="dropdown-menu border-0 shadow">
-      <li><a href="{{ route('user-payreqs.index') }}" class="dropdown-item">Submissions</a></li>
-      <li><a href="{{ route('user-payreqs.realizations.index') }}" class="dropdown-item">Realizations</a></li>
-      @can('akses_anggarans')
-      <li><a href="{{ route('user-payreqs.anggarans.index') }}" class="dropdown-item">RAB</a></li>
-      @endcan
-      <li><a href="{{ route('user-payreqs.histories.index') }}" class="dropdown-item">Histories</a></li>
-      @canany(['request_faktur', 'update_faktur'])
-      <li><a href="{{ route('user-payreqs.fakturs.index') }}" class="dropdown-item">Faktur</a></li>
-      @endcan
-      <li class="dropdown-divider"></li>
-      @can('akses_reports')
-      <li><a href="{{ route('reports.index') }}" class="dropdown-item">Reports</a></li>
-      @endcan
+        <li><a href="{{ route('user-payreqs.index') }}" class="dropdown-item">Submissions</a></li>
+        <li><a href="{{ route('user-payreqs.realizations.index') }}" class="dropdown-item">Realizations</a></li>
+        @can('akses_anggarans')
+            <li><a href="{{ route('user-payreqs.anggarans.index') }}" class="dropdown-item">RAB</a></li>
+        @endcan
+        <li><a href="{{ route('user-payreqs.histories.index') }}" class="dropdown-item">Histories</a></li>
+        @canany(['request_faktur', 'update_faktur'])
+            <li><a href="{{ route('user-payreqs.fakturs.index') }}" class="dropdown-item">Faktur</a></li>
+        @endcan
+        <li class="dropdown-divider"></li>
+        @can('akses_dokumen_upload')
+            <li><a href="{{ route('cashier.dokumen.index') }}" class="dropdown-item">Upload Dokumen</a></li>
+        @endcan
+        @can('akses_reports')
+            <li><a href="{{ route('reports.index') }}" class="dropdown-item">Reports</a></li>
+        @endcan
     </ul>
 </li>
