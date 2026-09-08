251af7d1cad6ade9f7cdbc3df42aaec6c527d4d4
Rachman J <rachmanj@gmail.com>
2026-09-08 04:59:33 +0000

    ui: action button tabel list loan icon-only + tooltip (edit/installments/history/delete)

--- resources/views/accounting/loans/action.blade.php
+++ resources/views/accounting/loans/action.blade.php
@@ -1,24 +1,20 @@
 @hasanyrole('superadmin|admin|cashier')
     <div class="vj-inline-actions">
-        <a class="vj-action-item vj-action-item-xs vj-action-edit" href="{{ route('accounting.loans.edit', $model->id) }}" title="Edit">
+        <a class="vj-action-item vj-action-item-xs vj-action-edit" href="{{ route('accounting.loans.edit', $model->id) }}" title="Edit Loan">
             <i class="fas fa-edit"></i>
-            <span>edit</span>
         </a>
         <a class="vj-action-item vj-action-item-xs vj-action-export" href="{{ route('accounting.loans.show', $model->id) }}" title="View Installments">
             <i class="fas fa-list"></i>
-            <span>installments</span>
         </a>
         <a class="vj-action-item vj-action-item-xs vj-action-print" href="{{ route('accounting.loans.history', $model->id) }}" title="View History">
             <i class="fas fa-history"></i>
-            <span>history</span>
         </a>
         @if ($model->installments->count() == 0)
             <form action="{{ route('accounting.loans.destroy', $model->id) }}" method="POST" class="vj-action-item-form">
                 @csrf @method('DELETE')
                 <button type="submit" class="vj-action-item vj-action-item-xs vj-action-cancel"
-                    onclick="return confirm('Are You sure You want to delete this record?');" title="Delete">
+                    onclick="return confirm('Are You sure You want to delete this record?');" title="Delete Loan">
                     <i class="fas fa-trash"></i>
-                    <span>delete</span>
                 </button>
             </form>
         @endif
